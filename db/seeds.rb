8cc150a61ea08e20771a15be157f8ebd52fc9c0e
Chuey Okoye <chuey9218@gmail.com>
2024-09-25 17:50:18 +0100

    mvp redone

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create(name: "Epicure", address: "75008 Paris", category: "french")
+Restaurant.create(name: "Le Meurice", address: "75001 Paris", category: "french")
+Restaurant.create(name: "Sushi Samba", address: "New York", category: "japanese")
